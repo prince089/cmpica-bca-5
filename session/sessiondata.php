d3ff1adc0ef44712998e32600b603e44e50c6b9b
Prince_hirapara <61899471+prince089@users.noreply.github.com>
2021-11-03 16:37:15 +0530

    set the varible in session

--- session/sessiondata.php
+++ session/sessiondata.php
@@ -1,2 +1,12 @@
-<?php
+
+ <?php
+// Start the session
+session_start();
+//for set the session varible
+$_SESSION["name"] = "joker";
+$_SESSION["age"] = "36";
+echo "Session variables are set.";
+
+
 ?>
+
